0eecdbfb374edb0fcc0d1bbfc6f81d07f93e41e3
M.P. Korstanje <rien.korstanje@gmail.com>
2026-07-02 15:14:41 +0200

    Extract properties to cucumber.properties so they work with IDEs too

--- src/test/java/com/example/project/StepDefinitions.java
+++ src/test/java/com/example/project/StepDefinitions.java
@@ -13,10 +13,10 @@ public class StepDefinitions {
         belly.eat(cukes);
     }
 
-    @When("I wait {int} hour")
+    @When("I wait {int} seconds")
     public void i_wait_hour(Integer int1) {
         // Write code here that turns the phrase above into concrete actions
-        throw new TestAbortedException("TODO");
+        throw new TestAbortedException("TODO: Implement me");
     }
 
 }
